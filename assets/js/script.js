81acb14f8eb99c64e7bc5098b9ea0323f614c727
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-16 18:44:21 +0100

    Implemented Carrozza and Codice Cp code
    Added a Math.floor number generated randomly for both, Carrozza and Codice Cp (used input with a return functions)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,24 @@
-// genero variabili associate all id ----> card_top
+
+
 var btnGenera = document.getElementById('btn_genera');
 // console.log(btnGenera);
 var btnAnnulla = document.getElementById('btn_annulla');
 // console.log(btnAnnulla);
 
+// genero variabili associate all id ----> card_top
+
+
+
+var fullName = document.getElementById('full-name');
+var kmToRun = document.getElementById('km');
+var fascia = document.getElementById ('fascia');
+
+
 // genero variabili associate all id ----> card_bottom
 
 
 var bottomFullName = document.getElementById('nome_passeggero');
-var bottomOfferta = document.getElementById('sconto');
+var bottomOfferta = document.getElementById('offerta');
 var bottomCarrozza = document.getElementById('carrozza');
 var bottomCodiceCp = document.getElementById('codice_cp');
 var bottomFinalTicketPrice = document.getElementById('ticket_price');
@@ -22,28 +32,28 @@ btnGenera.addEventListener('click',
 
 function() {
 
+    // MOSTRARE CARD_BOTTOM
     document.getElementById('your_ticket').style.display = 'block';
 
-//
 
-// CALCOLO NOME PASSEGGERO IN CARD_BOTTOM
-
-var fullName = document.getElementById('full-name');
-// console.log(fullName.value);
+// assegno variabili agli id card_bottom based on Input value
 
 var fullNameInput = fullName.value;
 // console.log(fullNameInput);
-document.getElementById('nome_passeggero').innerHTML = fullNameInput;
 
-
-var kmToRun = document.getElementById('km');
 var kmToRunInput = kmToRun.value;
 // console.log(kmToRunInput);
 
-var fascia = document.getElementById ('fascia');
 var fasciaInput = fascia.value;
+// console.log(fasciaInput);
+
+
+// CALCOLO NOME PASSEGGERO IN CARD_BOTTOM
+
+document.getElementById('nome_passeggero').innerHTML = fullNameInput;
+
 console.log(fasciaInput);
-// console.log(fascia);
+
 
 // CALCOLO PREZZO BIGLIETTO CARD_BOTTOM
 
@@ -56,17 +66,31 @@ if (fasciaInput == "minorenne") {
     ticketCost = ticketCost - (ticketCost * 0.2);
     // console.log(ticketCost);
     bottomFinalTicketPrice.innerHTML = ticketCost.toFixed(2) + "&euro;";
+    bottomOfferta.innerHTML = 'Under';
 } 
 else if (fasciaInput == "over65") {
     ticketCost = ticketCost - (ticketCost * 0.4);
     bottomFinalTicketPrice.innerHTML = ticketCost.toFixed(2) + "&euro;";
+    bottomOfferta.innerHTML = 'Over65';
+    // console.log(bottomOfferta);
 }
 else {
     bottomFinalTicketPrice.innerHTML = ticketCost.toFixed(2) + "&euro;";
+    bottomOfferta.innerHTML = "";
 }
 
 
+// CALCOLO NUMERO CARROZZA
 
+bottomCarrozza.innerHTML = Math.floor(Math.random() * 10);
+
+// CALCOLO CODICE CP
+
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+bottomCodiceCp.innerHTML = getRndInteger (90000, 99999);
 
 
 }
