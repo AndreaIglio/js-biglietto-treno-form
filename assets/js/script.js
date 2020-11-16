339f6f3d113bf479c78fdcdc1fda63bf130a947a
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-16 17:50:49 +0100

    Improved js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,77 @@
-// Variabili con valore
+// genero variabili associate all id ----> card_top
+var btnGenera = document.getElementById('btn_genera');
+// console.log(btnGenera);
+var btnAnnulla = document.getElementById('btn_annulla');
+// console.log(btnAnnulla);
 
 
-// Variabili senza valore
+// var fullNameInput = fullName.Value;
+// var kmInput = kmToRun.value;
+// var fasciaInput = fascia.value;
+
+// prezzo biglietto per km
+
+
+var kmPrice = 0.21;
+var ticketCost = kmPrice * km;
+// console.log(ticketCost);
+
+
+// genero variabili associate all id ----> card_bottom
+
+
+var bottomFullName = document.getElementById('nome_passeggero');
+var bottomSconto = document.getElementById('sconto');
+var bottomCarrozza = document.getElementById('carrozza');
+var bottomCodiceCp = document.getElementById('codice_cp');
+var bottomFinalTicketPrice = document.getElementById('ticket_price');
+
+
+
+
+// Event Listener
+
+btnGenera.addEventListener('click',
+
+function() {
+
+//
+var fullName = document.getElementById('full-name');
+// console.log(fullName.value);
+
+var fullNameInput = fullName.value;
+// console.log(fullNameInput);
+document.getElementById('nome_passeggero').innerHTML = fullNameInput;
+
+
+var kmToRun = document.getElementById('km');
+var kmToRunInput = kmToRun.value;
+// console.log(kmToRunInput);
+
+var fascia = document.getElementById ('fascia');
+var fasciaInput = fascia.value;
+console.log(fasciaInput);
+// console.log(fascia);
+
+
+
+
+}
+
+
+)
+
+
+
+
+btnAnnulla.addEventListener('click',
+
+function() {
+
+document.getElementById('your_ticket').style.display = 'none';
+
+
+}
+
+
+)
